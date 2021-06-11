884a5efac8bd2f006d4d51ab727e304a531f152b
ckf42 <31157514+ckf42@users.noreply.github.com>
2021-06-11 12:21:35 +0800

    updated py/expandKaTexMacro

--- py/expandKaTeXMacro.py
+++ py/expandKaTeXMacro.py
@@ -1,52 +1,96 @@
 import argparse
 import re
 import pathlib as path
-from personalPylib import findMatchBrackets as findMatchBrackets
+from collections import deque
+from personalPylib import findThisMatchBracket as findBracket
 
-filePath = r"D:\Acer\Documents\sync-work\progress.md"
-jsonPath = r"D:\Acer\Documents\sync-work\umm_KaTeX_compatible.json"
+parser = argparse.ArgumentParser()
+parser.add_argument('--md', type=str, help="Path to target md file")
+parser.add_argument('--json', type=str, help="Path to KaTeX macro json file")
+parser.add_argument('--out', type=str, help="Path to output file")
+args = parser.parse_args()
+
+filePath = path.Path((args.md
+                      if args.md is not None
+                      else input("Enter target path to markdown file:\n")
+                      ).strip('\'\" '))
+if not filePath.is_file() or filePath.suffix != '.md':
+    input("md is not a vaild path to a markdown file")
+    exit()
+
+jsonPath = path.Path((args.json
+                      if args.json is not None
+                      else input("Enter path to ref json file:\n")
+                      ).strip('\'\" '))
+if not jsonPath.is_file() or jsonPath.suffix != '.json':
+    input("json is not a vaild path to a json file")
+    exit()
+
+outputPath = (path.Path(args.out.strip('\'\" '))
+              if args.out is not None
+              else filePath.parent.joinpath(filePath.stem
+                                            + '_portable'
+                                            + filePath.suffix))
 
 fileContent = None
-with open(filePath, 'rt') as f:
+with filePath.open('rt', encoding='utf-8') as f:
     fileContent = f.read()
-bracketIdx = {
-    match[1]: match[2]
-    for match in findMatchBrackets(fileContent, '{}')
-}
 
 macroDict = dict()
-with open(jsonPath, 'rt') as f:
+with jsonPath.open('rt', encoding='utf-8') as f:
     for line in f:
         if line.startswith('    \"'):
-            matchObj = re.match(r'\s+"(\\\\.+?)":\s*"(.+?)",?\s*$',
-                                line).groups()
-            matchObj = (matchObj[0], matchObj[1].replace(r'\\', '\\'))
+            matchObj = tuple(m.replace(r'\\', '\\')
+                             for m in re.match(
+                                 r'\s+"(\\\\.+?)":\s*"(.+?)",?\s*$',
+                                 line).groups()
+                             )
             paraCount = max(int(i)
                             for i in [0, ] + re.findall(r'#(\d+)',
                                                         matchObj[1]))
             macroDict[matchObj[0]] = (matchObj[1], paraCount)
+macroList = tuple(macroDict.keys())
+for cmd in macroList:
+    macroDict[cmd] = macroDict[cmd] \
+        + (list(m for m in macroList if m in macroDict[cmd][0]), )
+    # content, paraCount, [depKeys]
 
-# how to keep track of location to modify?
-for key, cmdInfo in macroDict.items():
+macroQueue = deque(macroList)
+while len(macroQueue) != 0:
+    key = macroQueue[0]
+    macroQueue.popleft()
+    cmdInfo = macroDict[key]
+    if key not in fileContent:
+        continue
+    macroQueue.extend(cmdInfo[2])
     if cmdInfo[1] == 0:
-        fileContent = fileContent.replace(key, cmdInfo[0])
+        fileContent = re.sub(key.replace('\\', r'\\') + '\\b',
+                             cmdInfo[0].replace('\\', r'\\'),
+                             fileContent)
     else:
-        keyLen = len(key) - 1
-        keyIdx = tuple(match.start()
-                       for match in re.finditer(key + '\\W', fileContent))
-        for cmdIdx in keyIdx:
+        keyLen = len(key)
+        keyIdxList = tuple(match.start()
+                           for match
+                           in re.finditer(key.replace('\\', r'\\') + '\\b',
+                                          fileContent))
+        for startIdx in keyIdxList[::-1]:
+            replacementCmd = cmdInfo[0]
             paraDict = dict()
-            paraOpenBracketIdx = cmdIdx + keyLen
-            paraCloseBracketIdx = 0
-            for paraIdx in range(1, cmdInfo[1] + 1):
-                paraCloseBracketIdx = bracketIdx[paraOpenBracketIdx] + 1
-                paraDict[paraIdx] \
-                    = fileContent[paraOpenBracketIdx:paraCloseBracketIdx]
-                paraOpenBracketIdx = paraCloseBracketIdx
-            replacedCmd = cmdInfo[0]
-            for paraIdx in range(cmdInfo[1], 0, -1):
-                replacedCmd.replace(f'#{paraIdx}',
-                                    paraDict[paraIdx])
-            fileContent = fileContent[:cmdIdx] \
-                + replacedCmd \
-                + fileContent[paraCloseBracketIdx:]
+            startPos = startIdx + keyLen
+            endPos = 0
+            for i in range(1, cmdInfo[1] + 1):
+                endPos = findBracket(fileContent, startPos)
+                paraDict[i] = fileContent[startPos + 1:endPos]
+                startPos = endPos + 1
+            for i in range(cmdInfo[1], 0, -1):
+                replacementCmd = replacementCmd.replace(f'#{i}', paraDict[i])
+            fileContent = fileContent[:startIdx] \
+                + replacementCmd \
+                + fileContent[endPos + 1:]
+
+print(f"Writing to {str(outputPath)}")
+if outputPath.is_file():
+    print(f"{str(outputPath)} already exists")
+    input("Press Enter to overwrite file\n")
+with outputPath.open('wt', encoding='UTF-8') as f:
+    print(fileContent, file=f)

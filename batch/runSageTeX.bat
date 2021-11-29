34c1f434f9c95e66ca1e666b2a5a653373e01fe3
ckf42 <31157514+ckf42@users.noreply.github.com>
2021-11-29 18:36:07 +0800

    update bat/runsage

--- batch/runSageTeX.bat
+++ batch/runSageTeX.bat
@@ -11,15 +11,15 @@ if [%sageVersion%] == [] (
     set sageInstallName=
     for /f "usebackq tokens=* delims=" %%r in (`dir /b %sageInstallParentDir% ^| findstr /C:"SageMath"`) do set sageInstallName=%%r
     set sageVersion=!sageInstallName:~9,12!
-    set sageRuntimePath=%LOCALAPPDATA%\SageMath !sageVersion!\runtime
+    set sageRuntimePath=%LOCALAPPDATA%\!sageInstallName!\runtime
 )
 
 set sageCmd="%sageRuntimePath%\bin\bash.exe" -l "%sageRuntimePath%\opt\sagemath-%sageVersion%\sage"
 set dirPath=
 for /f "usebackq tokens=* delims=" %%r in (`"%sageRuntimePath%\bin\cygpath.exe" -m %~dp1`) do set dirPath=%%r
-set filePath=
-for /f "usebackq tokens=* delims=" %%r in (`echo %~n1`) do set filePath=%%r
+set fileName=
+for /f "usebackq tokens=* delims=" %%r in (`echo %~n1`) do set fileName=%%r
 
 echo SageMath Version %sageVersion%
-@REM echo %sageCmd% -c "os.chdir('%dirPath%'); load('%filePath%.sagetex.sage')"
-%sageCmd% -c "os.chdir('%dirPath%'); load('%filePath%.sagetex.sage')"
+@REM echo %sageCmd% -c "os.chdir('%dirPath%'); load('%fileName%.sagetex.sage')"
+%sageCmd% -c "os.chdir('%dirPath%'); load('%fileName%.sagetex.sage')"

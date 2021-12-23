329ae672114c06af1c31938986b815953f89febb
ckf42 <31157514+ckf42@users.noreply.github.com>
2021-12-23 15:50:49 +0800

    simplify batch/pipup

--- batch/pipup.bat
+++ batch/pipup.bat
@@ -1,21 +1,17 @@
 @echo off
 setlocal
 setlocal EnableDelayedExpansion
-set updateList=
+set pipUpdateCmd=pip install -U
 set hasChosenPackage=0
 for /f "usebackq tokens=1" %%s in (`pip list -o ^| fzf --header-lines=2 -m --sort -n 1`) do (
     set hasChosenPackage=1
-    if [!updateList!]==[] (
-        set updateList=%%s
-    ) else (
-        set updateList=!updateList! %%s
-    )
+    set pipUpdateCmd=!pipUpdateCmd! %%s
 )
 if !hasChosenPackage! geq 1 (
     echo Will execute the following command:
-    echo pip install -U !updateList!
+    echo !pipUpdateCmd!
     choice /C YN /M "Press Y to proceed, N for cancel"
     if not errorlevel 2 (
-        pip install -U !updateList!
+        !pipUpdateCmd!
     )
 )

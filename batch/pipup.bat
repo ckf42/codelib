3e7b6ba64c0534af8085476c9f29964cedd76d12
ckf42 <31157514+ckf42@users.noreply.github.com>
2022-01-06 13:53:11 +0800

    use python -m for batch/pipup when updating pip

--- batch/pipup.bat
+++ batch/pipup.bat
@@ -6,6 +6,9 @@ set hasChosenPackage=0
 for /f "usebackq tokens=1" %%s in (`pip list -o ^| fzf --header-lines=2 -m --sort -n 1`) do (
     set hasChosenPackage=1
     set pipUpdateCmd=!pipUpdateCmd! %%s
+    if "%%s"=="pip" (
+        set pipUpdateCmd=python -m !pipUpdateCmd!
+    )
 )
 if !hasChosenPackage! geq 1 (
     echo Will execute the following command:

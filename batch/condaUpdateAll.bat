1facc572a3d3674fb4153b9fc4c34180c552288b
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-02-09 23:40:39 +0800

    update batch/condaUp

--- batch/condaUpdateAll.bat
+++ batch/condaUpdateAll.bat
@@ -1,11 +1,21 @@
 @echo off
 
+setlocal
+
+for /F "usebackq" %%v in (`echo %CONDA_BAT%`) do @(
+    set condaBinPath=%%~dpv
+)
+
 for /F "usebackq tokens=1,2" %%n in (`%CONDA_BAT% info --envs`) do @(
     if [%%o] NEQ [] @(
         if [%%o] NEQ [conda] @(
             if [%%n] NEQ [base] @(
                 echo %%n
                 %CONDA_BAT% update --all --yes --name %%n
-            ))))
-
+                %CONDA_BAT% clean --all --yes
+                @REM del /F %condaBinPath%\..\envs\%%n\Scripts\*.c~
+            )
+        )
+    )
+)
 

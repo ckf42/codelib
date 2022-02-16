400693e6018f2b80fcfa803ac369b9f1e902aed6
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-02-16 23:28:41 +0800

    allow removing c~ files with /f in bat/condaUp

--- batch/condaUpdateAll.bat
+++ batch/condaUpdateAll.bat
@@ -10,10 +10,13 @@ for /F "usebackq tokens=1,2" %%n in (`%CONDA_BAT% info --envs`) do @(
     if [%%o] NEQ [] @(
         if [%%o] NEQ [conda] @(
             if [%%n] NEQ [base] @(
-                echo %%n
+                echo ----- %%n -----
                 %CONDA_BAT% update --all --yes --name %%n
                 %CONDA_BAT% clean --all --yes
-                @REM del /F %condaBinPath%\..\envs\%%n\Scripts\*.c~
+                if [%1] EQU [/f] @(
+                    dir %condaBinPath%\..\envs\%%n\Scripts\*.c~
+                    del /F %condaBinPath%\..\envs\%%n\Scripts\*.c~
+                )
             )
         )
     )

88b42c13362671ef7863ceff3ffc93318d2b96f4
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-06-02 14:02:26 +0800

    update py/whqr

--- py/wormholeQR.py
+++ py/wormholeQR.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('path',
                     type=str,
                     help="The path of file be sent by wormhole")
-parser.add_argument('--astext',
+parser.add_argument('--text',
                     action='store_true',
                     help="Send text by wormhole. "
                     "If specified, path will be treated as plain text")
@@ -19,26 +19,25 @@ parser.add_argument('--wormhole',
 args = parser.parse_args()
 
 os.environ["PYTHONUNBUFFERED"] = "1"
-p = subprocess.Popen(
-    [
-        args.wormhole,
-        'send',
-        f'--text={args.path}' if args.astext else args.path,
-    ],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    shell=True,
-    bufsize=0
-)
+cmdLst = [ args.wormhole, 'send' ] 
+if args.text:
+    cmdLst.append('--text')
+cmdLst.append(args.path)
 
 qrCodeObj = None
-while p.poll() is None:
-    for line in p.stdout:
-        line = line.decode(locale.getpreferredencoding())
-        print(line, end='')
-        if qrCodeObj is None and line.startswith('wormhole receive'):
-            whCode = line.split()[2]
-            qrCodeObj = qrcode.QRCode()
-            qrCodeObj.add_data(
-                f'wormhole:relay.magic-wormhole.io:4000?code={whCode}')
-            qrCodeObj.print_ascii(invert=True)
+with subprocess.Popen(cmdLst,
+                      stdout=subprocess.PIPE,
+                      stderr=subprocess.STDOUT,
+                      # shell=True,
+                      bufsize=0) as p:
+    while p.poll() is None:
+        for line in p.stdout:
+            line = line.decode(locale.getpreferredencoding())
+            print(line, end='')
+            if qrCodeObj is None and line.startswith('wormhole receive'):
+                whCode = line.split()[2]
+                qrCodeObj = qrcode.QRCode()
+                qrCodeObj.add_data(
+                    f'wormhole:relay.magic-wormhole.io:4000?code={whCode}')
+                qrCodeObj.print_ascii(invert=True)
+

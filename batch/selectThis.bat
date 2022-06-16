59f23ff9884b47e2ddbb92e9ede8f5c491281188
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-06-16 17:21:14 +0800

    fix bat/select

--- batch/selectThis.bat
+++ batch/selectThis.bat
@@ -5,12 +5,12 @@ set para=
 set multiPara=0
 
 :paraProcessing
-if [%1]==[] goto endParaProcessing
+if ["%~1"]==[""] goto endParaProcessing
 if [!para!]==[] (
-    for /f %%p in ("%1") do set para=%%p
+    for /f "tokens=*" %%p in ("%~1") do set para=%%p
 ) else (
     set multiPara=1
-    for /f %%p in ("%1") do set para=!para! %%p
+    for /f "tokens=*" %%p in ("%~1") do set para=!para! %%p
 )
 shift /1
 goto paraProcessing
@@ -21,7 +21,7 @@ if !multiPara! GEQ 1 (
     set para="!para!"
 )
 if exist !para! (
-    explorer /select,!para!
+    explorer /select,"!para!"
 ) else (
     echo !para! does not exist
 )

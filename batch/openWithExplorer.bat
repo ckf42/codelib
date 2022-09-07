675aefe29df54fc488b1f7644f565dbca37ceb45
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-09-07 19:35:52 +0800

    fix bat/sel, add bat/open

--- batch/openWithExplorer.bat
+++ batch/openWithExplorer.bat
@@ -2,12 +2,14 @@
 setlocal EnableDelayedExpansion
 
 set para=
+@REM set multiPara=0
 
 :paraProcessing
 if ["%~1"]==[""] goto endParaProcessing
 if [!para!]==[] (
     for /f "tokens=*" %%p in ("%~1") do set para=%%p
 ) else (
+    @REM set multiPara=1
     for /f "tokens=*" %%p in ("%~1") do set para=!para! %%p
 )
 shift /1
@@ -15,9 +17,11 @@ goto paraProcessing
 :endParaProcessing
 
 if [!para!]==[] exit /b
+@REM if !multiPara! GEQ 1 (
 set para="!para!"
+@REM )
 if exist !para! (
-    explorer /select,!para!
+    explorer !para!
 ) else (
     echo !para! does not exist
 )

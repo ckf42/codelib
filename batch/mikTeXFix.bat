2d9914a77f5de50886b990399f9835f9f94b0945
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-09-13 16:37:05 +0800

    fix bat/miktexfix, add dl for unicode-data

--- batch/mikTeXFix.bat
+++ batch/mikTeXFix.bat
@@ -3,6 +3,8 @@ setlocal
 cd %APPDATA%\MiKTeX\2.9
 set curlcmd=C:\Windows\System32\curl.exe -kLO
 set tarcmd=C:\Windows\System32\tar.exe -xf
+
+@REM latex package
 set targetPackage=l3backend l3kernel l3experimental l3packages
 for %%a in (%targetPackage%) do (
     echo ----- Downloading package %%a -----
@@ -12,6 +14,14 @@ for %%a in (%targetPackage%) do (
     echo ----- Cleaning up -----
     del %%a.tds.zip
 )
+@REM special case for unicode-data, generic package, different download link
+echo ----- Downloading package unicode-data -----
+%curlcmd% http://mirrors.ctan.org/install/macros/generic/unicode-data.tds.zip
+echo ----- Extracting downloaded archive -----
+%tarcmd% unicode-data.tds.zip
+echo ----- Cleaning up -----
+del unicode-data.tds.zip
+
 echo ----- Refreshing filename database -----
 initexmf --update-fndb --mkmaps --mklangs --mklinks --verbose
 echo ----- Done -----

5ef9b368c9f85d71522fcac1901066435ac22687
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-09-13 16:51:49 +0800

    make bat/miktexfix auto detect dllink

--- batch/mikTeXFix.bat
+++ batch/mikTeXFix.bat
@@ -1,28 +1,30 @@
 @echo off
-setlocal
-cd %APPDATA%\MiKTeX\2.9
-set curlcmd=C:\Windows\System32\curl.exe -kLO
-set tarcmd=C:\Windows\System32\tar.exe -xf
+setlocal EnableDelayedExpansion
 
-@REM latex package
-set targetPackage=l3backend l3kernel l3experimental l3packages
+set curlcmd=C:\Windows\System32\curl.exe -kLO
+set curlToStdOutCmd=C:\Windows\System32\curl.exe -kL
+set tarcmd=C:\Windows\System32\tar.exe -xf
+set ctanURL=https://ctan.org/pkg/
+
+cd %APPDATA%\MiKTeX\2.9
+
+set targetPackage=l3backend l3kernel l3experimental l3packages unicode-data
+set dlLink=
 for %%a in (%targetPackage%) do (
+    echo ----- Finding link for package %%a -----
+    for /F usebackq^ tokens^=2^ delims^=^" %%l in (`%curlToStdOutCmd% %ctanURL%%%a ^| findstr https://.*tds.zip`) do @(
+        set dlLink=%%l
+    )
     echo ----- Downloading package %%a -----
-    %curlcmd% http://mirrors.ctan.org/install/macros/latex/contrib/%%a.tds.zip
+    %curlcmd% !dlLink!
     echo ----- Extracting downloaded archive -----
     %tarcmd% %%a.tds.zip
     echo ----- Cleaning up -----
     del %%a.tds.zip
 )
-@REM special case for unicode-data, generic package, different download link
-echo ----- Downloading package unicode-data -----
-%curlcmd% http://mirrors.ctan.org/install/macros/generic/unicode-data.tds.zip
-echo ----- Extracting downloaded archive -----
-%tarcmd% unicode-data.tds.zip
-echo ----- Cleaning up -----
-del unicode-data.tds.zip
 
 echo ----- Refreshing filename database -----
 initexmf --update-fndb --mkmaps --mklangs --mklinks --verbose
 echo ----- Done -----
 cd %~dp0
+

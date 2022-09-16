e10ad6d3f6760048c151ba8bcd47e24dfe894cc2
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-09-16 08:53:17 +0800

    fix bat/updatesage

--- batch/updateSageTex.bat
+++ batch/updateSageTex.bat
@@ -1,5 +1,4 @@
 @echo off
-setlocal
 setlocal enabledelayedexpansion
 
 @rem settings
@@ -14,26 +13,49 @@ if [%sageVersion%] == [] (
     set sageVersion=!sageInstallName:~9,12!
     set sageRuntimePath=%LOCALAPPDATA%\!sageInstallName!\runtime
 )
-set sageTexStyPath=!sageRuntimePath!\opt\sagemath-!sageVersion!\local\share\texmf\tex\latex\sagetex\sagetex.sty
-
-if not exist "!sageTexStyPath!" (
+set sagetexPath=!sageRuntimePath!\opt\sagemath-!sageVersion!\local\share\texmf
+if not exist "!sagetexPath!\tex\latex\sagetex\sagetex.sty" (
     echo Cannot find sagetex.sty
     exit /b
 )
 echo sagePath
-echo "!sageTexStyPath!"
+echo "!sagetexPath!"
 
-if [%texTree%] == [] (
-    for /f %%p in ('kpsewhich -var-value TEXMFLOCAL') do (
+for /f %%l in ('where kpsewhich ^| find /c /v ""') do (
+    if %%l equ 0 (
+        echo No TeX distribution found
+        exit /b
+    )
+    if %%l geq 1 (
+        choice /M "Multiple TeX distribution found, install on all of them?"
+        if ERRORLEVEL 2 (
+            echo Exiting ...
+            exit /b
+        )
+    )
+)
+
+set kpsePath=
+for /f "tokens=*" %%k in ('where kpsewhich') do (
+    set kpsePath=%%k
+    set texhashPath=%%~dpktexhash
+    @REM echo !kpsePath!
+    @REM echo !texhashPath!
+    @REM echo "!kpsePath:~!" -var-value TEXMFLOCAL
+    for /f %%p in ('"!kpsePath:~!" -var-value TEXMFLOCAL') do (
         set texTree=%%~dpnp
     )
-    echo texTree
-    echo "!texTree!"
-)
-if not exist "!texTree!" (
-    echo Cannot locate texmf tree
-    exit /b
+    if [!texTree!]==[] (
+        echo %%k does not give a TEXMFLOCAL
+    ) else (
+        echo TEXHFMLOCAL at "!texTree!"
+        xcopy /E /-Y "!sagetexPath!" "!texTree!"
+        !texhashPath!
+    )
 )
 
-copy /-Y "!sageTexStyPath!" "!texTree!\tex\latex\sagetex\sagetex.sty"
+if not exist "!texTree!" (
+    echo Cannot locate any texmf tree
+    exit /b
+)
 

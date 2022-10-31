7501627628c7c3c58bc521bad4e9e5baa0b5830e
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-10-31 12:09:45 +0800

    add /-l for bat/book

--- batch/bookopen.bat
+++ batch/bookopen.bat
@@ -89,10 +89,11 @@ if !inQueryStrings! geq 1 (
         echo(/?, /h, /help, -h, --help  Display this help message and exit
         echo /r, /refresh               Reindex local files
         echo /f, /fetch                 Fetch remote index file
-        @REM echo /R, /rf, /r/f              Equivalent to /r /f
-        echo /R                         Equivalent to /r /f
+        @REM echo /R, /rf, /r/f              Equivalent to /f /r
+        echo /R                         Equivalent to /f /r
         echo /s, /search                Start searching after /r or /f
-        echo /l, /local                 Consider the local files only
+        echo /l, /local                 Consider local files only
+        echo /-l, /-local               Consider remote files only
         if %unknownParaAsQuery% geq 1 (
             echo --                         Stop parsing parameters and treat everything afterward as query string
         )
@@ -128,6 +129,10 @@ if !inQueryStrings! geq 1 (
         set localFileOnly=1
     ) else if "%1"=="/local" (
         set localFileOnly=1
+    ) else if "%1"=="/-l" (
+        set localFileOnly=-1
+    ) else if "%1"=="/-local" (
+        set localFileOnly=-1
     ) else if %unknownParaAsQuery% geq 1 (
         @REM special para: query strings
         if "%1"=="--" (
@@ -232,12 +237,15 @@ if not [!cliQueryStr!]==[] (
 )
 
 set fileFilterCmd=type %fileListPath%
-if !localFileOnly! equ 1 (
+if !localFileOnly! neq 0 (
     set findStrPattern=/C:!localPathArr[1]!
     for /l %%i in (2,1,%localPathCount%) do (
         set findStrPattern=!findStrPattern! /C:!localPathArr[%%i]!
     )
     set fileFilterCmd=!fileFilterCmd! ^| findstr !findStrPattern!
+    if !localFileOnly! equ -1 (
+        set fileFilterCmd=!fileFilterCmd! /V
+    )
 )
 if !scriptDebugFlag! neq 0 (echo !fileFilterCmd!)
 

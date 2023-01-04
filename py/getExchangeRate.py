95c093c04a81bab9b4e90560a13baf6ec0234278
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-01-04 22:40:09 +0800

    update py/getEx

--- py/getExchangeRate.py
+++ py/getExchangeRate.py
@@ -2,7 +2,15 @@ import requests as rq
 import datetime
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+        epilog="Notes on backends: "
+        "European Central Bank (ecb) (seemingly) only has API to convert from EUR. "
+        "Non-EUR conversion is done by converting to EUR first. "
+        "Result may not be accuracy. "
+        "Exchange Rate API (https://www.exchangerate-api.com) "
+        "is queried via their open access endpoint, "
+        "which has a 20-minute IP-based rate limit. "
+        "Props to the maintainers of these backends for providing these resources.")
 parser.add_argument('amount',
                     type=float,
                     nargs='?',
@@ -25,21 +33,19 @@ parser.add_argument('--decimal',
                     "Defaults to 2")
 parser.add_argument('--backend',
                     type=str,
-                    choices=['currency-api', 'ecb'],
+                    choices=('ecb', 'curr-api', 'er-api'),
                     default='ecb',
                     help="The backend API to query. "
-                    "Currently support currency-api and ecb (European Central Bank). "
                     "Affects what currencies are supported. "
-                    "For ECB, the rate is computed with EUR as exchange medium "
-                    "and may be inaccurate if neither currency is EUR. "
+                    "Currently support ecb (European Central Bank), "
+                    "curr-api (currency-api), and er-api (Exchange Rate API). "
                     "Defaults to ecb")
 args = parser.parse_args()
 
 
-def formatAmount(amount: float,
-                 decimalPlaces: int = args.decimal,
-                 separatePlaces: int = 3 if args.thousand else 4) -> str:
-    assert decimalPlaces >= 0, "Negative decimal places"
+def formatAmount(amount: float) -> str:
+    assert args.decimal >= 0, "Negative decimal places"
+    separatePlaces = 3 if args.thousand else 4
     isNeg = amount < 0
     amount = abs(amount)
     integerPart = str(int(amount))[::-1]
@@ -48,7 +54,7 @@ def formatAmount(amount: float,
                      for idx in range(0, len(integerPart), separatePlaces))
     outputRes = [','.join(p[::-1] for p in sepParts[::-1])]
     if decimalPart != 0:
-        outputRes.append('.' + str(round(decimalPart, decimalPlaces))[2:])
+        outputRes.append('.' + str(round(decimalPart, args.decimal))[2:])
     if isNeg:
         outputRes.insert(0, '-')
     return ''.join(outputRes)
@@ -57,6 +63,7 @@ def formatAmount(amount: float,
 def ecbQuery(fromCurr: str, toCurr: str) -> dict:
     # https://sdw-wsrest.ecb.europa.eu/help/
     def _ecbQueryBase(targetCurr: str) -> dict:
+        # only seems to have api to convert from eur
         ecbAddr = f'https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.{targetCurr.upper()}.EUR.SP00.A'
         reqPara = {
             'startPeriod': (datetime.date.today() - datetime.timedelta(days=14)).isoformat(),
@@ -73,25 +80,55 @@ def ecbQuery(fromCurr: str, toCurr: str) -> dict:
                 return {'rate': respVal, 'time': respTime}
             except:
                 raise RuntimeError("Failed parsing response")
+        elif resp.status_code == 404:
+            raise RuntimeError(f"Currency not supported: {targetCurr}")
         else:
             raise RuntimeError(f"Request returned with {resp.status_code}")
-    exch1 = _ecbQueryBase(fromCurr) if fromCurr.upper() != 'EUR' else {'rate': 1, 'time': datetime.date.today().isoformat()}
-    exch2 = _ecbQueryBase(toCurr) if toCurr.upper() != 'EUR' else {'rate': 1, 'time': datetime.date.today().isoformat()}
+    todayDate = datetime.date.today().isoformat()
+    exch1 = _ecbQueryBase(fromCurr) \
+            if fromCurr.upper() != 'EUR' \
+            else {'rate': 1, 'time': todayDate}
+    exch2 = _ecbQueryBase(toCurr) \
+            if toCurr.upper() != 'EUR' \
+            else {'rate': 1, 'time': todayDate}
     return {'rate': exch2['rate'] / exch1['rate'], 'time': min(exch1['time'], exch2['time'])}
 
 def currApiQuery(fromCurr: str, toCurr: str) -> dict:
-    # https://github.com/fawazahmed0/currency-api
+    # thanks https://github.com/fawazahmed0/currency-api for open access without needing a key
     apiAddr = f'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{fromCurr.lower()}/{toCurr.lower()}.min.json'
     resp = rq.get(apiAddr)
+    if resp.status_code == 403:
+        raise RuntimeError(f"Currencies may not be supported: {fromCurr.lower()} and {toCurr.lower()}")
     try:
         respJs = resp.json()
         return {'time': respJs['date'], 'rate': respJs[toCurr.lower()]}
-    except:
+    except rq.exceptions.JSONDecodeError:
         raise RuntimeError("Failed parsing response")
+
+def erApiQuery(fromCurr: str, toCurr: str) -> dict:
+    # thanks https://www.exchangerate-api.com for providing open access endpoint
+    # for doc, see https://www.exchangerate-api.com/docs/free
+    apiAddr = f'https://open.er-api.com/v6/latest/{fromCurr}'
+    resp = rq.get(apiAddr)
+    if resp.status_code == 429:
+        raise RuntimeError('Rate limited by Exchange Rate API. Please wait for about 20 minutes')
+    try:
+        respJs = resp.json()
+        if respJs['result'] != 'success':
+            raise RuntimeError(f"Request faild due to {respJs['error-type']}")
+        elif toCurr.upper() not in respJs['rates']:
+            raise RuntimeError(f"Has not data to convert from {fromCurr} to {toCurr}")
+        else:
+            return {'time': str(datetime.date.fromtimestamp(respJs['time_last_update_unix'])),
+                    'rate': respJs['rates'][toCurr.upper()]}
+    except rq.exceptions.JSONDecodeError:
+        raise RuntimeError("Failed parsing response")
+
 
 resDict = {
     'ecb': ecbQuery,
-    'currency-api': currApiQuery,
+    'curr-api': currApiQuery,
+    'er-api': erApiQuery,
 }[args.backend](args.fromCurr, args.toCurr)
 print(f"{formatAmount(resDict['rate'] * args.amount)} (on {resDict['time']})")
 

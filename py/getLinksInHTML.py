2057dd8ec0ca5b0be90672bc6470ea75615aba74
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-09-14 18:21:38 +0800

    update py/html

--- py/getLinksInHTML.py
+++ py/getLinksInHTML.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python3
 import argparse
-import requests as rq
 import urllib.parse
+
+import requests as rq
 from bs4 import BeautifulSoup, SoupStrainer
 
-parser = argparse.ArgumentParser()
-parser.add_argument('url', type=str, help="Target url")
-parser.add_argument('--port', '-p', type=int, help="Optional port number")
-args = parser.parse_args()
 
-if not args.url.startswith('http'):
-    args.url = '//' + args.url
-parsedUrl = urllib.parse.urlparse(args.url, 'http')
-baseUrl = urllib.parse.ParseResult('http',
-                                   parsedUrl.netloc + ('' if args.port is None else f':{args.port}'),
-                                   parsedUrl.path if parsedUrl.netloc else '',
-                                   *parsedUrl[3:]).geturl()
-forbidLink = ['#', 'javascript:', 'mailto:']
+def getArgs() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=str, help="Target url")
+    parser.add_argument('--port', '-p', type=int, help="Optional port number")
+    return parser.parse_args()
 
-res = rq.get(baseUrl)
-print("Status code:", res.status_code)
+strainer = SoupStrainer(lambda _, attrd: any(x in attrd for x in ('href', 'src')))
+strainer = SoupStrainer(
+        lambda _, attrd: \
+                'src' in attrd \
+                or ('href' in attrd \
+                    and not attrd['href'].startswith(('#', 'javascript:', 'mailto:')))
+        )
 
-for link in BeautifulSoup(res.content, features='html.parser').select('[href], [src]'):
-    if link.has_attr('href') and not any(link.get('href').startswith(forbid) for forbid in forbidLink):
-        print(urllib.parse.urljoin(baseUrl, link['href']))
-    elif link.has_attr('src'):
-        print(urllib.parse.urljoin(baseUrl, link['src']))
+def main():
+    args = getArgs()
+    if not args.url.startswith('http'):
+        args.url = '//' + args.url
+    parsedUrl = urllib.parse.urlparse(args.url, 'http')
+    baseUrl = urllib.parse.ParseResult(
+            'http',
+            parsedUrl.netloc + ('' if args.port is None else f':{args.port}'),
+            parsedUrl.path if parsedUrl.netloc else '',
+            *parsedUrl[3:]).geturl()
+    res = rq.get(baseUrl)
+    print("Status code:", res.status_code)
+    seenLinks: set[str] = set()
+    for link in BeautifulSoup(res.content,
+                              features='html.parser',
+                              parse_only=strainer):
+        if link.has_attr('href'):
+            seenLinks.add(urllib.parse.urljoin(baseUrl, link['href']))
+        elif link.has_attr('src'):
+            seenLinks.add(urllib.parse.urljoin(baseUrl, link['src']))
+    for link in sorted(seenLinks):
+        print(link)
+    print(f"Total: {len(seenLinks)}")
+
+if __name__ == '__main__':
+    main()
 

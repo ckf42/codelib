17863f54da5fdf286397a578c5495d4954054d68
KF Chan <31157514+ckf42@users.noreply.github.com>
2024-03-14 10:28:43 +0800

    update bat/book: add /fzf and /drive. index now sorted by localPath specified order, date, name, then remote

--- batch/bookopen.bat
+++ batch/bookopen.bat
@@ -19,17 +19,21 @@ set remoteIndexName=_bookIndex.txt
 @REM where remote index is stored
 set remoteIndexCacheDir=%USERPROFILE%\Downloads
 @REM path to aggregated local index
-set fileListPath=%USERPROFILE%\Desktop\aggregatedFileList.txt
+set fileListPath=%USERPROFILE%\books\_bookIndex.txt
 @REM file dir on remote
 set rcloneDrive=gd:books
 @REM base cmd for rclone
-set rcloneBaseCmd=rclone --drive-shared-with-me --progress
+set rcloneBaseCmd=rclone --progress
 @REM cmd used to get files from remote
-set rcloneCmd=%rcloneBaseCmd% copy %rcloneDrive%/
+set rcloneCmd=%rcloneBaseCmd% copy !rcloneDrive!/
 @REM cmd used to get index from from remote
-set rcloneIndexFetchCmd=%rcloneCmd%
+set rcloneIndexFetchCmd=!rcloneCmd!
 @REM allow unknown para as query? 0/1: F/T
 set unknownParaAsQuery=1
+@REM use preview window to show long filename? 0/1: F/T
+set useFzfPreview=1
+@REM the keybind for fzf preview window. Only used for useFzfPreview=1
+set fzfPreviewKey=ctrl-e
 REM script configs end here
 
 set remoteListPath=%remoteIndexCacheDir%\%remoteIndexName%
@@ -63,6 +67,9 @@ set needToStartSearch=0
 set cliQueryStr=
 set inQueryStrings=0
 set localFileOnly=0
+set extraFzfPara=
+set inFzfPara=0
+set inDriveOverride=0
 
 :handleParameters
 if [%1]==[] goto endHandleParameters
@@ -75,17 +82,29 @@ if !inQueryStrings! geq 1 (
     ) else (
         set cliQueryStr=!cliQueryStr! %1
     )
+) else if !inFzfPara! geq 1 (
+    if [!extraFzfPara!]==[] (
+        set extraFzfPara=%~1
+    ) else (
+        set extraFzfPara=!extraFzfPara! %~1
+    )
+    set inFzfPara=0
+) else if !inDriveOverride! geq 1 (
+    set rcloneDrive=%~1
+    set rcloneCmd=%rcloneBaseCmd% copy !rcloneDrive!/
+    set inDriveOverride=0
+    echo Using remote !rcloneDrive!
 ) else (
     @REM normal para handling
-    if "%1"=="/help" (
+    if "%~1"=="/help" (
         set needHelp=1
-    ) else if "%1"=="/h" (
+    ) else if "%~1"=="/h" (
         set needHelp=1
-    ) else if "%1"=="/?" (
+    ) else if "%~1"=="/?" (
         set needHelp=1
-    ) else if "%1"=="-h" (
+    ) else if "%~1"=="-h" (
         set needHelp=1
-    ) else if "%1"=="--help" (
+    ) else if "%~1"=="--help" (
         set needHelp=1
     )
     if !needHelp! equ 1 (
@@ -98,48 +117,54 @@ if !inQueryStrings! geq 1 (
         echo /s, /search                Start searching after /r or /f
         echo /l, /local                 Consider local files only
         echo /-l, /-local               Consider remote files only
+        echo /fzf                       The next parameter is to be passed to FZF
+        echo /drive                     The next parameter is rclone remote base override
         if %unknownParaAsQuery% geq 1 (
             echo --                         Stop parsing parameters and treat everything afterward as query string
         )
         @REM echo /d, /debug                 Print all debug messages
         goto endCleanUp
     )
-    if "%1"=="/debug" (
+    if "%~1"=="/debug" (
         set /a "scriptDebugFlag+=1"
-    ) else if "%1"=="/d" (
+    ) else if "%~1"=="/d" (
         set /a "scriptDebugFlag+=1"
-    ) else if "%1"=="/r" (
+    ) else if "%~1"=="/r" (
         set needToReindex=1
-    ) else if "%1"=="/refresh" (
+    ) else if "%~1"=="/refresh" (
         set needToReindex=1
-    ) else if "%1"=="/R" (
+    ) else if "%~1"=="/R" (
         set needToReindex=1
         set needToFetchRemoteIndex=1
-    @REM ) else if "%1"=="/rf" (
+    @REM ) else if "%~1"=="/rf" (
     @REM     set needToReindex=1
     @REM     set needToFetchRemoteIndex=1
-    @REM ) else if "%1"=="/r/f" (
+    @REM ) else if "%~1"=="/r/f" (
     @REM     set needToReindex=1
     @REM     set needToFetchRemoteIndex=1
-    ) else if "%1"=="/f" (
+    ) else if "%~1"=="/f" (
         set needToFetchRemoteIndex=1
-    ) else if "%1"=="/fetch" (
+    ) else if "%~1"=="/fetch" (
         set needToFetchRemoteIndex=1
-    ) else if "%1"=="/s" (
+    ) else if "%~1"=="/s" (
         set needToStartSearch=1
-    ) else if "%1"=="/search" (
+    ) else if "%~1"=="/search" (
         set needToStartSearch=1
-    ) else if "%1"=="/l" (
+    ) else if "%~1"=="/l" (
         set localFileOnly=1
-    ) else if "%1"=="/local" (
+    ) else if "%~1"=="/local" (
         set localFileOnly=1
-    ) else if "%1"=="/-l" (
+    ) else if "%~1"=="/-l" (
         set localFileOnly=-1
-    ) else if "%1"=="/-local" (
+    ) else if "%~1"=="/-local" (
         set localFileOnly=-1
+    ) else if "%~1"=="/fzf" (
+        set inFzfPara=1
+    ) else if "%~1"=="/drive" (
+        set inDriveOverride=1
     ) else if %unknownParaAsQuery% geq 1 (
         @REM special para: query strings
-        if "%1"=="--" (
+        if "%~1"=="--" (
             set inQueryStrings=1
         ) else if [!cliQueryStr!]==[] (
             set cliQueryStr=%1
@@ -202,7 +227,7 @@ if !needToReindex! equ 1 (
     if !scriptDebugFlag! neq 0 echo truncating local index
     break>%fileListPath%
     if !scriptDebugFlag! neq 0 echo rewriting local index
-    dir /s /b !localPath!>>%fileListPath%
+    dir /s /b /o:-dn !localPath!>>%fileListPath%
     for /f %%r in ('type %fileListPath% ^| find /c /v ""') do (
         set localFileCount=%%r
         echo Local file count: %%r
@@ -210,18 +235,18 @@ if !needToReindex! equ 1 (
 
     @rem find local file in remote and remove from cached remote index
     for %%f in ("%fileListPath%") do (
-        if !scriptDebugFlag! neq 0 (echo Writing %TEMP%\%%~nxf.tmp)
+        if !scriptDebugFlag! neq 0 (echo Writing local filenames in %TEMP%\%%~nxf.tmp)
         break>%TEMP%\%%~nxf.tmp
-        break>%TEMP%\%%~nxf.tmp.tmp
         for /f "tokens=*" %%l in (%%~f) do (
             >>%TEMP%\%%~nxf.tmp echo %%~nxl
         )
-        if !scriptDebugFlag! neq 0 (echo Writing %TEMP%\%%~nxf.tmp.tmp)
+        if !scriptDebugFlag! neq 0 (echo Writing filtered remote filepaths in %TEMP%\%%~nxf.tmp.tmp)
+        break>%TEMP%\%%~nxf.tmp.tmp
         if defined remoteIndexName (
             findstr /L /V /G:"%TEMP%\%%~nxf.tmp" %remoteListPath% >%TEMP%\%%~nxf.tmp.tmp
         )
-        type %%f >>%TEMP%\%%~nxf.tmp.tmp
-        type %TEMP%\%%~nxf.tmp.tmp >%%f
+        @REM type %%f >>%TEMP%\%%~nxf.tmp.tmp
+        type %TEMP%\%%~nxf.tmp.tmp >>%%f
         if !scriptDebugFlag! neq 0 (echo Cleaning up tmp files)
         del %TEMP%\%%~nxf.tmp %TEMP%\%%~nxf.tmp.tmp
     )
@@ -236,9 +261,17 @@ if not exist %fileListPath% (
     goto endCleanUp
 )
 set fzfCmd=fzf
-if not [!cliQueryStr!]==[] (
-    set fzfCmd=fzf -q "!cliQueryStr!"
+if !useFzfPreview! neq 0 (
+    set fzfCmd=!fzfCmd! --preview "echo {}" --preview-window "hidden:wrap" ^
+--bind "!fzfPreviewKey!:toggle-preview"
 )
+if not [!cliQueryStr!]==[] (
+    set fzfCmd=!fzfCmd! -q "!cliQueryStr!"
+)
+if not [!extraFzfPara!]==[] (
+    set fzfCmd=!fzfCmd! !extraFzfPara!
+)
+if !scriptDebugFlag! neq 0 (echo fzfCmd !fzfCmd!)
 
 set fileFilterCmd=type %fileListPath%
 if !localFileOnly! neq 0 (
@@ -253,7 +286,7 @@ if !localFileOnly! neq 0 (
 )
 if !scriptDebugFlag! neq 0 (echo !fileFilterCmd!)
 
-for /f "tokens=* delims=;" %%r in ('!fileFilterCmd! ^| !fzfCmd!') do set fzfResult=%%r
+for /f "usebackq tokens=* delims=;" %%r in (`!fileFilterCmd! ^| !fzfCmd!`) do set fzfResult=%%r
 if !scriptDebugFlag! neq 0 (echo fzfResult is !fzfResult!)
 if not [!fzfResult!]==[] (
     if exist "!fzfResult!" (
@@ -301,8 +334,8 @@ if not [!fzfResult!]==[] (
             goto endCleanUp
         )
         echo Downloading ...
-        if !scriptDebugFlag! neq 0 (echo %rcloneCmd%"!fzfResult!" !downloadPath!)
-        ( %rcloneCmd%"!fzfResult!" !downloadPath! ) && (
+        if !scriptDebugFlag! neq 0 (echo !rcloneCmd!"!fzfResult!" !downloadPath!)
+        ( !rcloneCmd!"!fzfResult!" !downloadPath! ) && (
             REM touching file
             for %%r in ("!fzfResult!") do @( copy /b "!downloadPath!\%%~nxr"+,, "!downloadPath!\" )
         ) && (

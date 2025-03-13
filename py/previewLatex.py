4bcdb7e305080371c4954bc3401cda0173c98b30
KF Chan <31157514+ckf42@users.noreply.github.com>
2025-03-13 15:14:48 +0800

    update py/preview
    backport from preview_sp, now also uses wezterm instead of manually
    outputting escaped seq (which does not work well except on mitty)

--- py/previewLatex.py
+++ py/previewLatex.py
@@ -1,10 +1,8 @@
 def previewLatex(inputExpression):
+    import pathlib
+    import shutil
     import subprocess
     import tempfile
-    import os
-    import shutil
-    import base64
-    import sys
 
     if shutil.which('pdflatex') is None:
         print("pdflatex not found")
@@ -15,7 +13,10 @@ def previewLatex(inputExpression):
 
     realTmpPath = '/tmp'
     if shutil.which('cygpath') is not None:
-        realTmpPath = subprocess.run(['cygpath', '-w', '/tmp'], capture_output=True, text=True).stdout.strip()
+        realTmpPath = subprocess.run(
+                ['cygpath', '-w', '/tmp'],
+                capture_output=True,
+                text=True).stdout.strip()
     texContent = '\n'.join((
         r'\documentclass{article}',
         r'\usepackage{amsmath, amssymb, amsfonts}',
@@ -29,43 +30,41 @@ def previewLatex(inputExpression):
         r'\end{document}',
     ))
     try:
-        fp = tempfile.NamedTemporaryFile('wt', delete=False, encoding='utf-8')
+        fp = tempfile.NamedTemporaryFile(
+                'wt',
+                delete=False,
+                encoding='utf-8',
+                prefix='previewLatex_')
         fpPath = fp.name.replace('/tmp', realTmpPath, 1)
         print(texContent, file=fp)
         fp.close()
-        texCompile = subprocess.run(['pdflatex', '-synctex=0', '-interaction=nonstopmode', fpPath],
-                                    cwd='/tmp',
-                                    stdout=subprocess.PIPE,
-                                    text=True,
-                                    encoding='UTF-8')
+        texCompile = subprocess.run(
+                ['pdflatex', '-synctex=0', '-interaction=nonstopmode', fpPath],
+                cwd='/tmp',
+                stdout=subprocess.PIPE,
+                text=True,
+                encoding='UTF-8')
         if '! LaTeX Error: File' in texCompile.stdout:
             pkgNameIdx = texCompile.stdout.find('! LaTeX Error: File `') + 21
             pkgNameSeg = texCompile.stdout[pkgNameIdx:]
             pkgNameEndIdx = pkgNameSeg.find("'")
             raise RuntimeError(f"Package {pkgNameSeg[:pkgNameEndIdx - 4]} not found")
-        subprocess.run(['pdftocairo', '-png', '-singlefile', fpPath + '.pdf', fpPath],
-                        cwd='/tmp',
-                        stdout=subprocess.PIPE)
-        if not os.path.exists(fp.name + '.png'):
+        subprocess.run(
+                ['pdftocairo', '-png', '-singlefile', fpPath + '.pdf', fpPath],
+                cwd='/tmp',
+                stdout=subprocess.PIPE)
+        pngPath = pathlib.Path(fp.name + '.png')
+        if not pngPath.exists():
             raise RuntimeError("pdftocairo failed")
-        fSize = os.path.getsize(fp.name + '.png')
         fContent = b''
-        with open(fp.name + '.png', 'rb') as imgF:
+        with pngPath.open('rb') as imgF:
             fContent = imgF.read()
-        outputSeq = b''.join((
-            b'\x1b]1337;File=size=',
-            bytes(str(fSize), 'ASCII'),
-            b';inline=1;height=auto:',
-            base64.b64encode(fContent),
-            b'\x07\x0a'
-        ))
-        print('')
-        sys.stdout.buffer.write(outputSeq)
-        print('')
+        subprocess.run(['wezterm', 'imgcat'], input=fContent)
     except Exception as e:
         print(e)
     finally:
         for f in ('.aux', '.log', '.pdf', '.png', ''):
-            if os.path.isfile(fp.name + f):
-                os.unlink(fp.name + f)
+            p = pathlib.Path(fp.name + f)
+            if p.is_file():
+                p.unlink()
 
--- py/readme.md
+++ py/readme.md
@@ -115,13 +115,16 @@ Some python scripts I have written. May be used in other scripts.
 
 ## previewLatex.py
 
-A function that renders `SageMath` expression (that can be `latex()`ed) into image and put it on console in [iTerm2 image sequence](https://iterm2.com/documentation-images.html)
+A function that renders `SageMath` expression (that can be `latex()`ed) into image and display it in the terminal ~~in [iTerm2 image sequence](https://iterm2.com/documentation-images.html)~~
 
-Requires the [`imgcat` package](https://pypi.org/project/imgcat/).
+~~Requires the [`imgcat` package](https://pypi.org/project/imgcat/).~~
+Now also uses [wezterm `imgcat`](https://wezfurlong.org/wezterm/cli/imgcat.html) to show image.
 
 The template latex content is edited from TeXStudio preview latex file. Compiling requires `amsmath`, `amssymb`, `amsfonts`, `preview`, `varwidth` packages
 
-Should work on all terminal emulators that support the iTerm2 image protocol, but currently on Windows only work with the default `mintty` terminal. Not sure why
+~~Should work on all terminal emulators that support the iTerm2 image protocol, but currently on Windows only work with the default `mintty` terminal. Not sure why~~
+
+**NOTE** target Python version 3.7.10 (with SageMath 9.3).
 
 ## previewLatex_sp.py
 
